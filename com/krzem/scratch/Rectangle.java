55b030dbb522313fd4f53bec1a8bfaf828321a36
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 16:41:09 +0100

    Push Update

---
+++ com/krzem/scratch/Rectangle.java
@@ -0,0 +1,38 @@
+package com.krzem.scratch;
+
+
+
+public class Rectangle{
+	public int x;
+	public int y;
+	public int w;
+	public int h;
+	public boolean neg=false;
+
+
+
+	public Rectangle(int x,int y,int w,int h){
+		this.x=x;
+		this.y=y;
+		this.w=w;
+		this.h=h;
+	}
+
+
+
+public class Rectangle{
+	public int x;
+	public int y;
+	public int w;
+	public int h;
+	public boolean neg=false;
+
+
+
+	public Rectangle(int x,int y,int w,int h){
+		this.x=x;
+		this.y=y;
+		this.w=w;
+		this.h=h;
+	}
+}
