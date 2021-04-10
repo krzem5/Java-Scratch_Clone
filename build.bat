53a3f74c5a777facc08ca4955f30e73348ca99e6
Krzem <krzem5.dev@gmail.com>
2021-04-10 10:58:14 +0200

    Push Update 04/10/2021, 10:57:23

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/scratch_clone/blocks/* com/krzem/scratch_clone/Main.java&&jar cvmf ../manifest.mf ../build/scratch_clone.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="scratch_clone.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/scratch_clone.jar test.xml
-:end
+python build.py %*
